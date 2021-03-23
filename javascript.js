d455de51979b2e2fad37f9dd5fc8e7c78c50d8c3
ismail-ahmad <80217756+ismail-ahmad@users.noreply.github.com>
2021-03-23 22:14:35 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function fun(){
+	var hello = "Hello there!";
+	document.getElementById("content").innerHTML = hello;
+}
+function fun2(){
+	var mouseleave = "";
+	document.getElementById("content").innerHTML = mouseleave;
+}
