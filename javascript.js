bf41b42d4e2e766a5c6ebf344e52f1947102b40d
ismail-ahmad <80217756+ismail-ahmad@users.noreply.github.com>
2021-03-23 22:36:06 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
 function fun(){
 	var hello = "Hello there!";
 	document.getElementById("content").innerHTML = hello;
+	document.getElementById("content").style.border = "1px solid blue" ;
+	document.getElementById("content").style.width = "50%" ;
+	document.getElementById("content").style.marginLeft = "25%" ;
+    document.getElementById("content").style.marginRight = "25%" ;
+	document.getElementById("content").style.borderRadius = "5px" ;
 }
 function fun2(){
 	var mouseleave = "";
 	document.getElementById("content").innerHTML = mouseleave;
+	document.getElementById("content").style.border = "hidden" ;
+	
 }
