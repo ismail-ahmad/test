8cafe5364b96e74665403054d70cbfd4c9ca40f0
ismail-ahmad <80217756+ismail-ahmad@users.noreply.github.com>
2022-10-05 16:22:01 +0500

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,2 @@
-import name from "./JavascriptE.js";
-document.getElementById("demo").innerHTML = "Hey " + name;
+import { name, age } from "./JavascriptE.js";
+document.getElementById("demo").innerHTML = "Hey " + name + age;
